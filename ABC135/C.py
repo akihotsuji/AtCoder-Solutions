84c77bdf4702c582e0b04d9afe403f3b44826dc0
akihotsuji <19960417akiho@gmail.com>
2023-06-23 15:02:50 +0900

    ABC135のC問題のコードを追加しました。

---
+++ ABC135/C.py
@@ -0,0 +1,8 @@
+import io,sys,os
+os.system("Cls")
+with open("HandInput.txt") as TxtOpen:
+    INPUT=TxtOpen.read() 
+sys.stdin=io.StringIO(INPUT)
+# --------------------------------------------------------
+
+https://atcoder.jp/contests/abc135/tasks/abc135_c
